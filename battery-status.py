9a51c5773f873e0eaf3f42f0571d77f0ea142dc8
jithesh kuyyalil <jk@thispc.jkhome>
2022-06-20 17:03:38 -0400

    updating battery-status.py, adding beep.wav

--- battery-status.py
+++ battery-status.py
@@ -7,13 +7,18 @@ from playsound import playsound
 # contains battery remaining power
 statusf = "/sys/class/power_supply/BAT0/capacity"
 with open(statusf) as f:
+    # keep checking the battery power every
+    # 5 min
     while True:
+        # battery remaining power
         pow_= f.read()
         print(pow_)
+        # when battery power less than 15%
+        # beep
         if float(pow_) < 15.0:
             print("power crisis!!")
             while True:
-                # beeps until exited
+                # beeps until killed
                 playsound('beep.wav')
                 time.sleep(2)
         time.sleep(5 * 60)
